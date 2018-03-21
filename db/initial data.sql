2fa61ebada998b1d2e49992b69106d57617832b7
ym <cmic2015@163.com>
2018-03-21 23:55:40 +0800

    数据库初始化脚本

---
+++ db/initial data.sql
@@ -0,0 +1,38 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.2.12-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table hb.daily_trade
+CREATE TABLE IF NOT EXISTS `daily_trade` (
+  `daily_trade_id` int(11) NOT NULL AUTO_INCREMENT,
+  `balance` int(11) NOT NULL DEFAULT 0 COMMENT '买入数量',
+  `symbol` varchar(20) NOT NULL,
+  `exec_time` varchar(5) NOT NULL COMMENT '执行时间，格式为HH:mm',
+  `t` float NOT NULL COMMENT '期望收益率百分数，比如期望收益率1.2%，这个值应该是1.2',
+  `buy_order_id` int(11) DEFAULT NULL COMMENT '买单id，挂买单成功后更新到该字段',
+  `buy_price` float DEFAULT NULL,
+  `sell_order_id` int(11) DEFAULT NULL COMMENT '卖单id，挂卖单成功后更新到该字段',
+  `status` int(11) NOT NULL COMMENT '当前状态',
+  PRIMARY KEY (`daily_trade_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table hb.daily_trade: ~0 rows (approximately)
+DELETE FROM `daily_trade`;
+/*!40000 ALTER TABLE `daily_trade` DISABLE KEYS */;
+INSERT INTO `daily_trade` (`daily_trade_id`, `balance`, `symbol`, `exec_time`, `t`, `buy_order_id`, `buy_price`, `sell_order_id`, `status`) VALUES
+	(3, 150, 'omg', '21:00', 1.3, NULL, NULL, NULL, 1),
+	(4, 12000, 'cmt', '23:00', 1.3, NULL, NULL, NULL, 1);
+/*!40000 ALTER TABLE `daily_trade` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- trade.py
+++ trade.py
@@ -12,7 +12,7 @@ def run_task():
 
     # 查询现有数量
     balance = get_symbol_balance('omg')
-
+    task_list = get_task_list()
 
     print("now is", time.time())
     print("balance", balance)
@@ -22,5 +22,38 @@ def run_task():
     s = sched.scheduler(time.time, time.sleep)
     s.enter(10, 0, run_task)
     s.run()
+
+
+def get_task_list():
+    # 打开数据库连接
+    db = pymysql.connect("localhost", "root", "password", "hb")
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+    # 使用 execute()  方法执行 SQL 查询
+    cursor.execute("select * from daily_trade")
+    # 使用 fetchone() 方法获取单条数据.
+    data = cursor.fetchall()
+
+    list = []
+    for row in data:
+        daily_trade = DailyTrade(row)
+        list.append(daily_trade)
+    # 关闭数据库连接
+    db.close()
+
+    return list
+
+# 日常交易任务
+class DailyTrade(object):
+    def __init__(self,row):
+        self.id = row[0]
+        self.balance = row[1]
+        self.symbol = row[2]
+        self.exec_time = row[3]
+        self.t = row[4]
+        self.buy_order_id = row[5]
+        self.buy_price = row[6]
+        self.sell_order_id = row[7]
+        self.status = row[7]
 
 run_task()
