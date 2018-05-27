c6bddede972ce3f3eeb28db5f455d259c9a590e1
Yu.miao <yu.miao@hinacom.com>
2018-05-27 20:33:31 +0800

    ...

--- g.py
+++ g.py
@@ -3,9 +3,10 @@ import json
 import time
 import sched
 import pymysql.cursors
+from api.HuobiServices import *
 
 
-def get_buy_order_list():
+def get_pending_buy_order_list():
     connection = pymysql.connect(host='localhost',
                                  user='root',
                                  password='password',
@@ -27,9 +28,9 @@ def get_buy_order_list():
 
 # 下单后更新状态
 def set_buy_order(order):
-
     now = datetime.datetime.now()
-    buy_time =  order["r_buy_time"]
+    buy_time = order["r_buy_time"]
+
     if now > buy_time:
         connection = pymysql.connect(host='localhost',
                                      user='root',
@@ -38,6 +39,20 @@ def set_buy_order(order):
                                      cursorclass=pymysql.cursors.DictCursor)
         try:
             with connection.cursor() as cursor:
+                # 下单
+                symbol = order["symbol"]
+                amount = order["amount"]
+                order_type = 'buy-limit'
+                price = 0
+                kline_list = get_kline_ex(symbol, "60min", 30)
+                for kline in kline_list:
+                    kline_time = kline["time"]
+                    if buy_time == kline_time:
+                        open = kline["open"]
+                        price = open
+
+                result = send_order(amount, '', symbol, order_type, price)
+
                 sql = "update order_item set status=2 where order_item_id=%s"
                 order_id = order["order_item_id"]
                 cursor.execute(sql, (order_id))
@@ -47,18 +62,17 @@ def set_buy_order(order):
         finally:
             connection.close()
 
+
 # 计数器，一个循环任务，总共让自己执行3次
 def run_task():
-
     # 打印信息
     print("run time is ", time.time())
-    order_list = get_buy_order_list();
 
-    print("current order list: ", len(order_list))
-
-    for order in order_list:
+    # 下买单
+    buy_order_list = get_pending_buy_order_list();
+    print("current order list: ", len(buy_order_list))
+    for order in buy_order_list:
         set_buy_order(order)
-
 
     # 只要没有让自己调用到第3次，那么继续重头开始执行本任务
     # 这里的delay 可以重新指定
