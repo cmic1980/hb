cdc4403aa10a9c2363ad28b98afb6b3438591efd
Yu.miao <yu.miao@hinacom.com>
2018-07-20 21:25:24 +0800

    ...

--- g.py
+++ g.py
@@ -33,17 +33,20 @@ def get_order_list_by_status(status):
 
 # 下单后更新状态
 def set_buy_order(order):
+    symbol = order["symbol"]
+    print("set buy order 1: ", symbol)
+
     now = datetime.datetime.now()
     buy_time = order["r_buy_time"]
-
     if now > buy_time:
+        print("set buy order 2: ", symbol)
+
         connection = connection = get_connection()
         try:
             with connection.cursor() as cursor:
                 # 下单
                 symbol = order["symbol"]
                 amount = order["amount"]
-                order_type = 'buy-limit'
                 buy_price = 0
                 kline_list = get_kline_ex(symbol, "60min", 30)
                 for kline in kline_list:
@@ -53,7 +56,7 @@ def set_buy_order(order):
                         buy_price = open
                         sell_price = (1 + 1.8 / 100) * buy_price
 
-                result = send_order(amount, '', symbol, order_type, buy_price)
+                result = send_order(amount, '', symbol, 'buy-limit', buy_price)
 
                 sql = "update order_item set status=2,order_id=%s,buy_price=%s,sell_price=%s where order_item_id=%s"
                 order_id = result["data"]
@@ -69,7 +72,11 @@ def set_buy_order(order):
 # 更新完全成交买单状态
 def update_buy_order_complete(order):
     order_id = order["order_id"]
+    symbol = order["symbol"]
+    print("update buy order complete 1: ", symbol)
+
     if order_id != -1:
+        print("update buy order complete 2: ", symbol)
         order = order_info(order_id)
         state = order["data"]["state"]
         if state == "filled":
@@ -88,8 +95,11 @@ def update_buy_order_complete(order):
 def cancel_buy_order(order):
     now = datetime.datetime.now()
     buy_time = order["r_buy_time"]
+    symbol = order["symbol"]
+    print("cancel buy order 1: ", symbol)
     buy_time = buy_time + datetime.timedelta(hours=1)
     if now > buy_time:
+        print("cancel buy order 2: ", symbol)
         order_id = order["order_id"]
         if order_id != -1:
             cancel_order(order_id)
@@ -107,10 +117,18 @@ def cancel_buy_order(order):
 
 # 下卖单
 def set_sell_order(order):
-
     order_id = order["order_id"]
+    sell_price = order["sell_price"]
+    symbol = order["symbol"]
+    print("set_sell_order 1: ", symbol)
     if order_id != -1:
         symbol = order["symbol"]
+        print("set_sell_order 2: ", symbol)
+        balance = get_symbol_balance(symbol.replace("eth",""))
+        # 如果余额大于0.01，挂卖单
+        if balance>0.01:
+            result = send_order(balance, '', symbol, 'sell-limit', sell_price)
+            pass
 
         # 完成的买单/取消的买单 下完买单后变为完成状态
         status = order["status"]
@@ -124,17 +142,12 @@ def set_sell_order(order):
                 connection.commit()
             finally:
                 connection.close()
-        pass
-
 
 # 计数器，一个循环任务，总共让自己执行3次
 def run_task():
     # 打印信息
-    print("run time is ", time.time())
-
     # 下买单
     order_list = get_order_list_by_status(1);
-    print("current order list: ", len(order_list))
     for order in order_list:
         set_buy_order(order)
 
